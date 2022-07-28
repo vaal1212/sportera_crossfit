eff85482d627ec8facc490aaea083fa3eaa2b0b3
vaal12 <qwe1@qwe.com>
2022-07-28 12:58:30 +0300

    Removing secret key from setting. Moved to .env file (and changed).

--- sportera_crossfit/settings.py
+++ sportera_crossfit/settings.py
@@ -9,16 +9,13 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import os
 
 from pathlib import Path
 
 from dotenv import load_dotenv
 
 load_dotenv()  # take environment variables from .env.
-# print("loaded env")
-# import os
-# print(f"SECRET:{os.getenv('SECRET_KEY')}")
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-((o=nifz+#luz0t-^mnu055en0_hk7s4v#$uilkjxkg0d%nv0s'
+# Secret key is moved to .env file and was changed as it was in previous commits. 
+# .env will not be included to the commits.
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
